0aac9866c5556546b113b7d27a8adbafe636f30d
Benjamin <35331377+Do660@users.noreply.github.com>
2025-01-28 14:39:38 +0100

    Update GmScreenCell.ts

--- src/module/classes/GmScreenCell.ts
+++ src/module/classes/GmScreenCell.ts
@@ -56,7 +56,7 @@ export class GmScreenCell {
       $(this.cellId).find('.gm-screen-grid-cell-title').text(this.title);
 
       //@ts-expect-error I inject cellId
-      const gridCellContent = $(this.cellId).find('.gm-screen-grid-cell-content');
+      const gridCellContent = $(this.cellId).find('[name="gm-screen-grid-cell-content-section"]');
       gridCellContent.html(html);
       //@ts-expect-error Yeah I know this isn't the best idea
       this._element = html;
